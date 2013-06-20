ad8c717eb909d0fcd5599d94af30c019fed51ef1
apps2web <apps2web@gmail.com>
2013-06-20 14:36:45 +0300

    Update server.js
    Actualizo el puerto para que coja el del entorno

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ http.createServer(function (req, res) {
   console.log('A new request arrives with HTTP headers ' + JSON.stringify(req.headers));
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Pluralsight\n');
-}).listen(1337);
+}).listen(process.env.PORT);
 console.log('Server running');
