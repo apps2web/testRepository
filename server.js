dcecab2976b6d52a7de9bef99a023f96476a932b
apps2web <apps2web@gmail.com>
2013-06-20 14:11:47 +0200

    update the server message

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   console.log('A new request arrives with HTTP headers ' + JSON.stringify(req.headers));
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Pluralsight\n');
+  res.end("Hello Pluralsight. I'm a javascript node app listening at " + process.env.IP + ":" + process.env.PORT + " \n");
 }).listen(process.env.PORT);
 console.log('Server running');
+
